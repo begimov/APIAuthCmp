664f5f904bb124e947d1136375dc3f00e5eabeda
begimov <begimov@gmail.com>
2018-01-28 15:37:32 +0300

    Message relation to User implemented

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Message extends Model
 {
-    //
+    protected $fillable = ['body'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
