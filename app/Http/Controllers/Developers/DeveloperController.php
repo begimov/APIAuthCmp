6687acd3698e1f3f4ba5f7b6c3d79c2b5131252e
begimov <begimov@gmail.com>
2018-01-30 11:30:19 +0300

    Developer controller created

---
+++ app/Http/Controllers/Developers/DeveloperController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Developers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DeveloperController extends Controller
+{
+    //
+}
